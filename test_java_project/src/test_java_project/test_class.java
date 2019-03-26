2791fb37b900923fc6e7265da0f20fe24530611f
Joseph Yoon <ricerich.gmail.com>
2019-03-27 06:08:42 +0900

    hotfix branch change a code

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -5,6 +5,7 @@ public class test_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world~! hotfix!");
+		System.out.println("i'm the hotfix~!");
 
 	}
 
