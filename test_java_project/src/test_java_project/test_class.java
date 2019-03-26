4fc7be67b443b9010976c4ca55542f2bfda0e88f
Joseph Yoon <ricerich.gmail.com>
2019-03-27 05:59:33 +0900

    master branch change a code

--- test_java_project/src/test_java_project/test_class.java
+++ test_java_project/src/test_java_project/test_class.java
@@ -5,6 +5,8 @@ public class test_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hello world~! hotfix!");
+		System.out.println("i'm the master!");
+		
 
 	}
 
